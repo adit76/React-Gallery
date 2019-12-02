51b0b1574ff80ba552b5aab1e7e38d82f0b019c1
Adit Sapkota <aditsapkota.as@gmail.com>
2019-12-02 16:23:46 +0545

    Update

--- src/components/AddImg.js
+++ src/components/AddImg.js
@@ -24,13 +24,13 @@ class AddImg extends Component {
   render() {
     return(
       <form onSubmit={this.onSubmit} className="newImg">
-        <input type ="text" name="title" placeholder="Image Title" value={this.state.title} onChange={this.onChange}/>
-        <input type ="text" name="x1" placeholder="X1 Co-ordinates (Pixels)" value={this.state.x1} onChange={this.onChange}/>
-        <input type ="text" name="y1" placeholder="Y1 Co-ordinates (Pixels)" value={this.state.y1} onChange={this.onChange}/>
-        <input type ="text" name="x2" placeholder="X2 Co-ordinates (Pixels)" value={this.state.x2} onChange={this.onChange}/>
-        <input type ="text" name="y2" placeholder="Y2 Co-ordinates (Pixels)" value={this.state.y2} onChange={this.onChange}/>
+        <input type ="text" name="title" placeholder="Image Title" value={this.state.title} onChange={this.onChange} required/>
+        <input type ="text" name="x1" placeholder="X1 Co-ordinates (Pixels)" value={this.state.x1} onChange={this.onChange} required/>
+        <input type ="text" name="y1" placeholder="Y1 Co-ordinates (Pixels)" value={this.state.y1} onChange={this.onChange} required/>
+        <input type ="text" name="x2" placeholder="X2 Co-ordinates (Pixels)" value={this.state.x2} onChange={this.onChange} required/>
+        <input type ="text" name="y2" placeholder="Y2 Co-ordinates (Pixels)" value={this.state.y2} onChange={this.onChange} required/>
         <span style={{fontSize: '25px'}}>
-        <input type="text" name="url" placeholder="Enter Image URL (https://www.freeiconspng.com/uploads/address-icon-1.png)" style={{width: 450}} value={this.state.url} onChange={this.onChange}/>
+        <input type="text" name="url" placeholder="Enter Image URL (https://www.freeiconspng.com/uploads/address-icon-1.png)" style={{width: 450}} value={this.state.url} onChange={this.onChange} required/>
         </span>
         <input type="submit" value="SUBMIT" className="btnSubmit" />
       </form>
